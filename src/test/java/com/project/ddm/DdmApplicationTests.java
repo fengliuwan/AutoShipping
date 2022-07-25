c924dd8d9ce5ae2afdb8f745da4a31aa70230449
fengliuwan <fengwuyan23@gmail.com>
2022-07-25 13:29:59 -0400

    started project base file

---
+++ src/test/java/com/project/ddm/DdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ddm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
