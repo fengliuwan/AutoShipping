3096d86b8e8994c056440ab7b155e1d492971385
xiaoxi <94241956+xxxxiaoxi@users.noreply.github.com>
2022-07-27 16:56:21 -0400

    Add files via upload

---
+++ src/main/java/com/project/ddm/repository/StationRepository.java
@@ -0,0 +1,12 @@
+package com.project.ddm.repository;
+
+import com.project.ddm.model.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StationRepository extends JpaRepository<Station, Integer> {
+    List<Station> findAll();
+}
